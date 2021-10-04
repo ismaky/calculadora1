bb6bf9970b4e6c65f76091799d9843da5f21d881
Ismael <ismael@Ismaels-MacBook-Pro.local>
2021-10-04 00:28:18 -0300

    meu projeto

--- Calculadora1/Calculadora1/ViewController.swift
+++ Calculadora1/Calculadora1/ViewController.swift
@@ -11,6 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        func cd(){
+            
+        }
         // Do any additional setup after loading the view.
     }
 
